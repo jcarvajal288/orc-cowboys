94914f21f48b45ea80ae8886108e436ac92b7d9c
Max Carvajal <jcarvajal288@gmail.com>
2025-07-30 15:35:55 -0500

    Add second cowboy

--- Characters/Cowboy/cowboy.gd
+++ Characters/Cowboy/cowboy.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
+@export var sprite_texture: Texture2D
+
 @onready var animation_player = $AnimationPlayer
 
 var speed = 75
 
 func _ready() -> void:
+	if sprite_texture:
+		$Sprite2D.texture = sprite_texture
 	$StateMachine.init(self)
 
 func _physics_process(delta: float) -> void:
