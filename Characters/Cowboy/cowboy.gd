5d5ff96d6af08e9edebf3793fe07e1cecd72ed47
Max Carvajal <jcarvajal288@gmail.com>
2025-07-30 15:11:50 -0500

    Cowboy moves around

--- Characters/Cowboy/cowboy.gd
+++ Characters/Cowboy/cowboy.gd
@@ -2,5 +2,18 @@ extends CharacterBody2D
 
 @onready var animation_player = $AnimationPlayer
 
+var speed = 75
+
 func _ready() -> void:
 	$StateMachine.init(self)
+
+func _physics_process(delta: float) -> void:
+	$StateMachine.process_physics(delta)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	$StateMachine.process_input(event)
+
+
+func _process(delta: float) -> void:
+	$StateMachine.process_frame(delta)
