828281a3d222b1d2e5907f51f4279bf149d1f87e
Max Carvajal <jcarvajal288@gmail.com>
2025-07-30 15:52:46 -0500

    Cowboys move independently

--- Characters/Components/Director/InputDirector/input_director.gd
+++ Characters/Components/Director/InputDirector/input_director.gd
@@ -1,5 +1,6 @@
 class_name InputDirector extends Director
 
+@export var wasd_movement: bool
 
 func bind_8_way(input_direction: Vector2) -> Vector2:
 	return Vector2(
@@ -9,4 +10,7 @@ func bind_8_way(input_direction: Vector2) -> Vector2:
 
 
 func _physics_process(_delta: float) -> void:
-	movement_vector = bind_8_way(Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down"))
+	if wasd_movement:
+		movement_vector = bind_8_way(Input.get_vector("wasd_left", "wasd_right", "wasd_up", "wasd_down"))
+	else:
+		movement_vector = bind_8_way(Input.get_vector("arrow_left", "arrow_right", "arrow_up", "arrow_down"))
