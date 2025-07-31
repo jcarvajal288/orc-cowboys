c422bf88f1e7b773894493b1835d63540882a84d
Max Carvajal <jcarvajal288@gmail.com>
2025-07-30 22:12:44 -0500

    Fix idle facing bug

--- Entities/Characters/Cowboy/character_animations.gd
+++ Entities/Characters/Cowboy/character_animations.gd
@@ -6,13 +6,13 @@ func play_with_facing(anim_name: String) -> void:
 	self.play(anim_name + facing)
 
 func set_facing(direction: Vector2):
-	if direction.x >= 0:
-		if direction.y >= 0:
+	if direction.x > 0:
+		if direction.y > 0:
 			facing = "DownRight"
 		else:
 			facing = "UpRight"
 	elif direction.x < 0:
-		if direction.y >= 0:
+		if direction.y > 0:
 			facing = "DownLeft"
 		else:
 			facing = "UpLeft"
