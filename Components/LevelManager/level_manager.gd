d7fdb467eb7dda17dfe2b4921ca8ca1582754a10
Max Carvajal <jcarvajal288@gmail.com>
2025-08-01 15:28:02 -0500

    Remove pig when scoring it

--- Components/LevelManager/level_manager.gd
+++ Components/LevelManager/level_manager.gd
@@ -1,6 +1,28 @@
 extends Node
 
 
+func _ready() -> void:
+	$ScoreArea.body_entered.connect(score_critter)
+	disable_score_area(true)
+
+
+func disable_score_area(b: bool) -> void:
+	$ScoreArea/CollisionPolygon2D.disabled = b
+	$ScoreArea/LoopPolygon.visible = !b
+
+
+
 func score_loop(loop: Array) -> void:
 	print("score loop")
-	$LoopPolygon.polygon = loop
+	$ScoreArea/LoopPolygon.polygon = loop
+	$ScoreArea/CollisionPolygon2D.polygon = loop
+	disable_score_area(false)
+	$ScoreTimer.start(0.1)
+	await $ScoreTimer.timeout
+	disable_score_area(true)
+
+
+func score_critter(body: Node2D) -> void:
+	if is_instance_of(body, Critter):
+		print("scored " + body.name)
+		body.queue_free()
