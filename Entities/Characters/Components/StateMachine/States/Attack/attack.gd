7be563903e9e646731685d10ed22100f2f97a669
Max Carvajal <jcarvajal288@gmail.com>
2025-08-02 15:34:52 -0500

    Add sfx to lion attack

--- Entities/Characters/Components/StateMachine/States/Attack/attack.gd
+++ Entities/Characters/Components/StateMachine/States/Attack/attack.gd
@@ -2,10 +2,12 @@ extends State
 
 @export var state_machine: StateMachine
 @export var idle_state: State
+@export var sound_effect: AudioStreamPlayer2D
 
 func enter() -> void:
 	super()
 	subject.velocity = Vector2.ZERO
+	sound_effect.play()
 
 
 func _on_animation_player_animation_finished(anim_name:StringName) -> void:
