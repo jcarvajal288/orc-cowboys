8ab7d06ecd6a96629f1693bded564ff3909df0f6
Max Carvajal <jcarvajal288@gmail.com>
2025-08-02 22:15:20 -0500

    Fixed bug where lions could go through ropes

--- Entities/Characters/Critters/Pig/pig.gd
+++ Entities/Characters/Critters/Pig/pig.gd
@@ -3,3 +3,16 @@ class_name Pig extends Critter
 func _init() -> void:
 	speed = 20
 	score = 50
+
+
+func _process(_delta: float) -> void:
+	if Global.rng.randi() % 5000 == 0:
+		play_sound()
+
+
+func play_sound() -> void:
+	var sounds = get_children().filter(func(child):
+		return child is AudioStreamPlayer2D
+	)
+	sounds.shuffle()
+	sounds[0].play()
